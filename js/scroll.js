89aa1239c88a68118212ae0ab9efdda467afd70e
Sanches Santos <55898372+sanchesfranklin@users.noreply.github.com>
2019-10-31 23:00:52 -0300

    Primeiro projeto
    Adicionando arquivos

---
+++ js/scroll.js
@@ -0,0 +1,10 @@
+
+$('nav a').click(function (e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+        targetOffset = $(id).offset().top,
+        menuHeight = $('nav').innerHeight();
+    $('html, body').animate({
+        scrollTop: targetOffset - menuHeight
+    }, 500);
+});
